449842d1cde8d38780dd146b44d84390c2477f47
Pablo Cuesta <pablocsaa@gmail.com>
2021-02-21 14:58:15 +0100

    errors

--- Ejercicios/Ejercicio 3/main.c
+++ Ejercicios/Ejercicio 3/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
-int main(){
-    FILE *f=NULL;
+int main(int argc, char **argv){
+    FILE *f = NULL;
+    int error_code;
 
-    if((f=fopen("/etc/shadow","r"))==NULL){
-        perror("Error al abrir el archivo");
-    };
-    
-    if(f!=NULL)
+    if (argc != 2){
+        printf("enter one argument (filename)\n");
+        return EXIT_FAILURE;
+    }
+
+    f = fopen(argv[1], "r");
+    error_code = errno;
+    printf("Código del error: %d\n", error_code);
+
+    perror("Error con código al abrir el archivo");
+
+    if (f != NULL)
         fclose(f);
 
     return 0;
